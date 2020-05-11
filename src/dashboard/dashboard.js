9d56c9497e0b9af5d90109aee04ee73f7a8e1607
Omanshu480 <omanshumahawar.181co237@nitk.edu.in>
2020-05-12 01:51:55 +0530

    Showing Chat List

--- src/dashboard/dashboard.js
+++ src/dashboard/dashboard.js
@@ -1,13 +1,61 @@
 import React from 'react';
+import ChatListComponent from '../chatlist/chatList';
+
+const firebase = require("firebase");
 
 class DashboardComponent extends React.Component {
+
+    constructor() {
+        super();
+        this.state = {
+            selectedChat: null,
+            newChatFormVisible: false,
+            email: null,
+            chats: []
+        };
+    }
 
     render() {
         return (
             <div>
-                Hello World from DashBoard
+                <ChatListComponent
+                    history={this.props.history}
+                    newChatBtnFn={this.newChatButtonClicked}
+                    selectChatFn={this.selectChat}
+                    chats={this.state.chats}
+                    userEmail={this.state.email}
+                    selectedChatIndex={this.state.selectedChat}
+                ></ChatListComponent>
             </div>
         );
+    }
+
+    selectChat = (chatIndex) => {
+        console.log("Selected a Chat", chatIndex);
+    }
+
+    newChatButtonClicked = () => this.setState({ newChatFormVisible: true, selectChat: null });
+
+    componentDidMount = () => {
+        firebase.auth().onAuthStateChanged(async _usr => {
+            if (!_usr) {
+                this.props.history.push('/login');
+            }
+            else {
+                await firebase
+                    .firestore()
+                    .collection('chats')
+                    .where('users', 'array-contains', _usr.email)
+                    .onSnapshot(async res => {
+                        const chats = res.docs.map(_doc => _doc.data());
+                        await this.setState({
+                            email: _usr.email,
+                            chats: chats
+                        });
+                        console.log(this.state);
+                    })
+            }
+        })
     }
 
 }
